cab0587f8ae461c5ca77d17a5190673525099da6
Stylianos Grigoriadis <stylgrig@yahoo.gr>
2024-08-11 20:21:31 +0300

    Second attempt to create a differential_equation

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ def differential_equation(xn0,iterations):
     K=100
     list_of_results = []
     dx=0
-    print('Hello')
+
     for i in range(iterations):
-        print(i)
+
         xn0 = xn0+dx
+        print(xn0)
         list_of_results.append(xn0)
-        dx = r*xn0(1-xn0/K)
+        dx = r*xn0*(1-xn0/K)
+        print(dx)
 
     return list_of_results
 
-list_of_values_5 = differential_equation(5,100)
-list_of_values_35 = differential_equation(35,100)
-list_of_values_70 = differential_equation(70,100)
-list_of_values_100 = differential_equation(100,100)
-list_of_values_120 = differential_equation(120,100)
+list_of_values_5 = differential_equation(5,1000)
+list_of_values_35 = differential_equation(35,1000)
+list_of_values_70 = differential_equation(70,1000)
+list_of_values_100 = differential_equation(100,1000)
+list_of_values_120 = differential_equation(120,1000)
 
 plt.plot(list_of_values_5, label='list_of_values_5')
-plt.plot(list_of_values_35, label='list_of_values_35')
-plt.plot(list_of_values_70, label='list_of_values_70')
+# plt.plot(list_of_values_35, label='list_of_values_35')
+# plt.plot(list_of_values_70, label='list_of_values_70')
 plt.plot(list_of_values_100, label='list_of_values_100')
 plt.plot(list_of_values_120, label='list_of_values_120')
 plt.legend()
